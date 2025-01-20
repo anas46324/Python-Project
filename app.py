39eed36c79a666c18fda9232a17dbb36a60e59c4
AnasAhmed <43697270+anas46324@users.noreply.github.com>
2025-01-20 15:35:24 +0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+################# Even Odd number Code ###############
+
+oddcount = 0; evencount= 0
+for number in range(1, 100):
+    if number %2 != 0 :
+        oddcount += 1
+    else:
+        evencount +=1
+print(f"We have {evencount} even number" " and " f"We have {oddcount} odd number")
+
+################# Even Odd number Code ###############
+
